21b77b75e0ef40ad3359a684cca41cc3e6f624f9
Aryansemwal011 <aryansemwal32@gmail.com>
2025-02-17 23:08:58 +0530

    "changes"

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
+from datetime import datetime
 
 app = Flask(__name__)
 app.secret_key = 'super secret key'
@@ -11,17 +12,22 @@ db = SQLAlchemy(app)
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100))
+    work = db.Column(db.String(100))
+    date = db.Column(db.Date, nullable=False, default=datetime.utcnow)
+    time = db.Column(db.Time, nullable=False)
     complete = db.Column(db.Boolean)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)  # Add this line
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(200), nullable=False)
+    todos = db.relationship('Todo', backref='user', lazy=True)  # Add this line
 
 @app.route("/")
 def home():
     if 'user_id' in session:
-        todo_list = Todo.query.all()
+        todo_list = Todo.query.filter_by(user_id=session['user_id']).all()  # Filter by user_id
         return render_template("base.html", todo_list=todo_list)
     return redirect(url_for('login'))
 
@@ -65,7 +71,12 @@ def add():
     if 'user_id' not in session:
         return redirect(url_for('login'))
     title = request.form.get("title")
-    new_todo = Todo(title=title, complete=False)
+    work = request.form.get("work")
+    date_str = request.form.get("date")
+    time_str = request.form.get("time")
+    date_obj = datetime.strptime(date_str, '%Y-%m-%d').date()
+    time_obj = datetime.strptime(time_str, '%H:%M').time()
+    new_todo = Todo(title=title, work=work, date=date_obj, time=time_obj, complete=False, user_id=session['user_id'])  # Add user_id here
     db.session.add(new_todo)
     db.session.commit()
     return redirect(url_for("home"))
@@ -74,7 +85,7 @@ def add():
 def update(todo_id):
     if 'user_id' not in session:
         return redirect(url_for('login'))
-    todo = Todo.query.filter_by(id=todo_id).first()
+    todo = Todo.query.filter_by(id=todo_id, user_id=session['user_id']).first()  # Add user_id filter
     todo.complete = not todo.complete
     db.session.commit()
     return redirect(url_for("home"))
@@ -83,7 +94,7 @@ def update(todo_id):
 def delete(todo_id):
     if 'user_id' not in session:
         return redirect(url_for('login'))
-    todo = Todo.query.filter_by(id=todo_id).first()
+    todo = Todo.query.filter_by(id=todo_id, user_id=session['user_id']).first()  # Add user_id filter
     db.session.delete(todo)
     db.session.commit()
     return redirect(url_for("home"))
